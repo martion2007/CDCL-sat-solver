db675de7c47014e57958f8e372f8a7f86b6a43d6
Marco Scully <120331337+martion2007@users.noreply.github.com>
2026-09-24 19:47:27 +0100

    Update main.py

--- Src/main.py
+++ Src/main.py
@@ -1,1 +1,14 @@
+from time import perf_counter
+from Solver import Solver
 
+
+start = perf_counter()
+solver = Solver()
+for i in range(1, 1001):
+    print(f"Number: {i}")
+    solver.readfile(f"../Benchmarks/uf100-430/uf100-0{i}.cnf")
+    solver.solve()
+    #print(solver.checker())
+
+end = perf_counter()
+print(end - start)
